365a4342b26ba3f137d5231db3819764e8176298
Alexander Trefonas <atrefonas@gmail.com>
2022-08-29 13:17:42 -0400

    fix to show all filesystems

--- webserver/lib/get_data.ts
+++ webserver/lib/get_data.ts
@@ -1,7 +1,39 @@
 import axios from 'axios';
 
-export default async function getData() {
+
+export interface FileSystemStatus {
+  file_system: string,
+  size: string,
+  used: string,
+  available: string,
+  use_percent: string,
+  mounted_on: string
+}
+
+export interface StatusData {
+  _id: string,
+  _rev: string,
+  external_ipaddr: string,
+  hostname: string,
+  datetime: string,
+  unix_time: number,
+  server_name: string,
+  filesystem_status: Array<FileSystemStatus>
+}
+
+export async function getData() {
   const url = '/data'
-  const data = (await axios.get(url)).data;
-  return data;
+  const data:Array<StatusData> = (await axios.get(url)).data;
+  console.log(JSON.stringify(data));
+  const retdata:Array<StatusData> = [];
+
+  data.forEach(server => {
+    server.filesystem_status.forEach(filesystem => {
+      const entry = {...server};
+      entry.filesystem_status = [ filesystem ];
+      retdata.push(entry);
+    });
+  });
+
+  return retdata;
 }
